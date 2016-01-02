15171ab63e83af9246ec664444092e8ac413e1f3
Erik Olofsson <erik.olofsson@hansoft.com>
2016-01-02 09:39:40 +0000

    [Malterlib] Update license
    [grafted history before @55251]
    
    Copied from Perforce
     Change: 55250

---
+++ Source/Malterlib_InterProcess_SharedMemory.h
@@ -0,0 +1,97 @@
+// Copyright © 2015 Hansoft AB 
+// Distributed under the MIT license, see license text in LICENSE.Malterlib
+
+#pragma once
+
+namespace NMib
+{
+	namespace NInterProcess
+	{
+		class CSharedMemory
+		{
+			void *m_pHandle;
+			void *m_pMemory;
+			CSharedMemory(CSharedMemory const &);
+			CSharedMemory &operator = (CSharedMemory const &);
+			void fp_Destroy()
+			{
+				if (m_pHandle)
+					NSys::fg_InterProcess_MemFree(m_pHandle, m_pMemory);
+			}
+		public:
+			CSharedMemory(CSharedMemory &&_Other)
+				: m_pHandle(_Other.m_pHandle)
+				, m_pMemory(_Other.m_pMemory)
+			{
+				_Other.m_pHandle = nullptr;
+				_Other.m_pMemory = nullptr;
+			}
+			CSharedMemory &operator = (CSharedMemory &&_Other)
+			{
+				fp_Destroy();
+				m_pHandle = _Other.m_pHandle;
+				m_pMemory = _Other.m_pMemory;
+				_Other.m_pHandle = nullptr;
+				_Other.m_pMemory = nullptr;
+				return *this;
+			}
+			CSharedMemory()
+				: m_pHandle(nullptr)
+				, m_pMemory(nullptr)
+			{
+			}
+			CSharedMemory(ch8 const *_pName, mint _Size)
+				: m_pHandle(nullptr)
+			{
+				m_pHandle = NSys::fg_InterProcess_MemAlloc(_pName, _Size, m_pMemory);
+			}
+			~CSharedMemory()
+			{
+				fp_Destroy();
+			}
+
+			void *f_GetMem()
+			{
+				return m_pMemory;
+			}
+
+			void const *f_GetMem() const
+			{
+				return m_pMemory;
+			}
+
+			operator void *() const
+			{
+				return m_pMemory;
+			}
+
+			void *f_GetHandle() const
+			{
+				return m_pHandle;
+			}
+		};
+
+		template <typename t_CType>
+		class TCSharedMemory : protected CSharedMemory
+		{
+		public:
+			using CSharedMemory::f_GetHandle;
+			TCSharedMemory(ch8 const *_pName)
+				: CSharedMemory(_pName, sizeof(t_CType))
+			{
+			}
+			TCSharedMemory()
+			{
+			}
+
+			t_CType &f_Get()
+			{
+				return *((t_CType *)this->f_GetMem());
+			}
+			t_CType const &f_Get() const
+			{
+				return *((t_CType const *)this->f_GetMem());
+			}
+		};
+	}
+}
---
+++ Test/Test_Malterlib_InterProcess_SharedMemory.cpp
@@ -0,0 +1,44 @@
+// Copyright © 2015 Hansoft AB 
+// Distributed under the MIT license, see license text in LICENSE.Malterlib
+
+#include <Mib/InterProcess/SharedMemory>
+
+namespace
+{
+	using namespace NMib::NTest;
+
+	class CMemory_Tests : public CTest
+	{
+	public:
+
+		CMemory_Tests()
+		{
+		}
+
+		void f_DoTests()
+		{
+			void *pMemory;
+			NMib::NInterProcess::CSharedMemory SharedMem;
+			try
+			{
+				SharedMem = NMib::NInterProcess::CSharedMemory("Global\\MalterlibSharedMemoryTest", 1024);
+				pMemory = SharedMem;
+			}
+			catch (NMib::NException::CException const &)
+			{
+				try
+				{
+					SharedMem = NMib::NInterProcess::CSharedMemory("Local\\MalterlibSharedMemoryTest", 1024);
+					pMemory = SharedMem;
+				}
+				catch (NMib::NException::CException const &)
+				{
+				}
+			}
+		}		
+	};
+
+	DMibTestRegister(CMemory_Tests, Malterlib::InterProcess);
+
+}
+
